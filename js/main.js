7809e51a1835c20df59edb93890ab53f60738020
sananabbasov <sanan_abbasov@list.ru>
2023-12-23 16:47:38 +0400

    second part

--- js/main.js
+++ js/main.js
@@ -1,19 +1,41 @@
 $(document).keypress(function (event) {
     let position = $("#bird").position()
     let gameZone = $("#gameZone").css(['width']).width.replace("px", "")
+    let basketPosition = $(".basket").first().position()
 
     if (event.keyCode == 97) {
         if (position.left != 0) {
             $("#bird").css("left", `${position.left - 20}px`)
+            $(".bait").first().css("left", `${position.left - 20 + 100}px`)
             $("#bird").css("transform", `rotateY(-180deg)`)
         }
     } else if (event.keyCode ==100) {
         if (gameZone > position.left + 245) {
             $("#bird").css("left", `${position.left + 20}px`)
+            $(".bait").first().css("left", `${position.left + 20 + 100}px`)
             $("#bird").css("transform", `rotateY(0deg)`)
 
         }
+    }else if(event.keyCode == 32){
+        let eaglePositon = position.left
+        let res = eaglePositon - basketPosition.left
+        if (res > -50 && res < 50) {
+         
+            let getPoint = localStorage.getItem("point")
+            $(".bait").first().css("top", `90%`)
+
+            localStorage.setItem("point", ++getPoint)
+        }
+        setTimeout(() =>{
+            $(".bait").first().css("display", `none`)
+        },2000)
+        setTimeout(() =>{
+            $(".bait").first().css("top", `160px`)
+            $(".bait").first().css("display", `block`)
+        },3000)
+      
     }
+
 
 
 
@@ -26,4 +48,9 @@ setInterval(() => {
 
 }, 5000)
 
+
+
+
+
+// local, session and cookie stroges
 
